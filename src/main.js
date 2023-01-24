17001ac07cc22bd6b29907061d77a1847647428c
アレク <58715574+BK-222@users.noreply.github.com>
2023-01-24 16:50:20 +0000

    Outsourced router to separate file.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router.js'
 
 import './assets/main.css'
 
 import { createStore } from 'vuex'
-import { createRouter, createWebHistory } from 'vue-router'
 
 const store = createStore();
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: App }
-  ]
-});
 
 const app = createApp(App);
 app.use(store);

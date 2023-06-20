b9bc98b91bcaaccf4bb6e39375646592a79b82ed
BK-222 <58715574+BK-222@users.noreply.github.com>
2023-06-20 03:10:34 +0000

    None

--- src/pages/about/about.vue
+++ src/pages/about/about.vue
@@ -1,4 +1,4 @@
 <template>
-  <p class="text-3xl text-blue-700">World Hold On! ! ! </p>
+  <p class="text-3xl text-blue-700">World Hold On! ! ! Future</p>
   <router-link to='/'>main</router-link>
 </template>

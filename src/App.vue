795b82f370e4f1bef8b087cd232b5e8fce3ab8c5
letsgetjscoding <>
2023-06-20 01:11:16 +0000

    little update

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
+  <navbar></navbar>
   <router-view></router-view>
 </template>
+
+<script>
+  import Navbar from './components/Navbar.vue'
+  export default {
+    components: {
+      Navbar
+    } 
+  }
+</script>
